6fc0a3f3b4960e1719cabd09db812b0d7ebfe23a
Daniel Schäfer <s9dlscae@stud.uni-saarland.de>
2014-06-18 22:40:41 +0200

    first class added

---
+++ impl/raytracer/geom/Plane.java
@@ -0,0 +1,22 @@
+package raytracer.geom;
+
+import raytracer.core.Hit;
+import raytracer.core.Obj;
+import raytracer.core.def.LazyHitTest;
+import raytracer.math.Constants;
+import raytracer.math.Point;
+import raytracer.math.Ray;
+import raytracer.math.Vec2;
+import raytracer.math.Vec3;
+
+class Plane extends BBoxedPrimitive {
+	private final Vec3 n;
+	private final float d;
+
+	public Plane(final Point a, final Point b, final Point c){
+		/* TODO BBox super */
+		
+	}
+
+
+}
