63e26589664c29404ad26b682f090b4a56f8cf52
Daniel Schäfer <s9dlscae@stud.uni-saarland.de>
2014-06-26 23:08:48 +0200

    most party of BVH ready

--- impl/raytracer/core/def/BVH.java
+++ impl/raytracer/core/def/BVH.java
@@ -1,5 +1,6 @@
 package raytracer.core.def;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import raytracer.core.Hit;
@@ -14,18 +15,41 @@ import raytracer.math.Vec3;
  * Represents a bounding volume hierarchy acceleration structure
  */
 public class BVH extends BVHBase {
+
+	private final List<Obj> objList;
+
 	public BVH() {
-		// BVH ist eine Liste von Objekten! (Baum)
-		
-		// TODO: implement this
-		throw new UnsupportedOperationException(
-				"This method has not yet been implemented.");
+		objList = new LinkedList<Obj>();
 	}
 
 	@Override
 	public BBox bbox() {
-		// Implement this method
-		return this.bbox();
+		BBox result = BBox.EMPTY;
+
+		for (final Obj obj : objList) {
+
+			Float kl0 = Math.min(result.getMin().get(0), obj.bbox().getMin()
+					.get(0));
+			Float kl1 = Math.min(result.getMin().get(1), obj.bbox().getMin()
+					.get(1));
+			Float kl2 = Math.min(result.getMin().get(2), obj.bbox().getMin()
+					.get(2));
+			Float kl3 = Math.min(result.getMin().get(3), obj.bbox().getMin()
+					.get(3));
+			Float gr0 = Math.max(result.getMax().get(0), obj.bbox().getMax()
+					.get(0));
+			Float gr1 = Math.max(result.getMax().get(1), obj.bbox().getMax()
+					.get(1));
+			Float gr2 = Math.max(result.getMax().get(2), obj.bbox().getMax()
+					.get(2));
+			Float gr3 = Math.max(result.getMax().get(3), obj.bbox().getMax()
+					.get(3));
+
+			result = new BBox(new Point(kl0, kl1, kl2, kl3), new Point(gr0,
+					gr1, gr2, gr3));
+		}
+
+		return result;
 	}
 
 	/**
@@ -36,8 +60,8 @@ public class BVH extends BVHBase {
 	 */
 	@Override
 	public void add(final Obj prim) {
-		// Implement this method
-		this.add(prim);
+		objList.add(prim);
+
 	}
 
 	/**
@@ -46,7 +70,8 @@ public class BVH extends BVHBase {
 	@Override
 	public void buildBVH() {
 		// Implement this method
-		this.buildBVH();
+		// ruft nachher alles auf
+
 	}
 
 	@Override
@@ -58,30 +83,75 @@ public class BVH extends BVHBase {
 
 	@Override
 	public int calculateSplitDimension(final Vec3 size) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException(
-				"This method has not yet been implemented.");
+		if ((size.x() <= size.y()) & (size.x() <= size.z()))
+			return 1;
+		if ((size.y() <= size.x()) & (size.y() <= size.z()))
+			return 2;
+		else
+			return 3;
 	}
 
 	@Override
 	public void distributeObjects(final BVHBase a, final BVHBase b,
 			final int splitdim, final float splitpos) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException(
-				"This method has not yet been implemented.");
+		
+		List<Obj> helpList = this.getObjects();
+		if (splitdim==1){
+			// x - achsen split
+			for (Obj o : helpList) {
+				Point m = o.bbox().getMin().add((o.bbox().getMax().sub(o.bbox().getMin()).scale(0.5f)));
+				if (m.x()<splitpos)
+					a.add(o);
+				else 
+					b.add(o);
+				
+			}
+		}
+		if (splitdim==2){
+			// y - achsen split
+			for (Obj o : helpList) {
+				Point m = o.bbox().getMin().add((o.bbox().getMax().sub(o.bbox().getMin()).scale(0.5f)));
+				if (m.y()<splitpos)
+					a.add(o);
+				else 
+					b.add(o);
+			}
+		}
+		if (splitdim==3){
+			// z - achsen split
+			for (Obj o : helpList) {
+				Point m = o.bbox().getMin().add((o.bbox().getMax().sub(o.bbox().getMin()).scale(0.5f)));
+				if (m.z()<splitpos)
+					a.add(o);
+				else 
+					b.add(o);
+			
+			}
+		}	
 	}
 
 	@Override
 	public Hit hit(final Ray ray, final Obj obj, final float tmin,
 			final float tmax) {
-		// Implement this method
-		return obj.bbox().hit(ray, tmin, tmax);
+		// implemened
+		if (this.bbox().hit(ray, tmin, tmax).hits()) {
+			
+			List<Obj> helpList = this.getObjects();
+
+			for (Obj o : helpList) {
+
+				Hit helpHit = o.hit(ray, obj, tmin, tmax);
+				if (helpHit.hits()) {
+					return helpHit;
+				}
+			}
+		}
+		return Hit.No.get();
 	}
 
 	@Override
 	public List<Obj> getObjects() {
-		// TODO Implement this method
-		throw new UnsupportedOperationException(
-				"This method has not yet been implemented.");
+		// implemened
+		return objList;
 	}
 }
