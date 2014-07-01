634a5132d6cbfead8712fcb5c3617df8c360ec59
Daniel Schäfer <s9dlscae@stud.uni-saarland.de>
2014-07-01 14:30:20 +0200

    Phong comments deleted

--- impl/raytracer/shade/Phong.java
+++ impl/raytracer/shade/Phong.java
@@ -18,7 +18,6 @@ import raytracer.math.Vec3;
 
 public class Phong implements Shader {
 
-	// Test
 
 	private final Shader inner;
 	private final Color ambient;
@@ -38,8 +37,6 @@ public class Phong implements Shader {
 	@Override
 	public Color shade(Hit hit, Trace trace) {
 
-		// TODO abfangen von wird das Objekt überhaupt getroffen fraglich! +
-		// TODO Pixelfehler beheben
 
 		Color result = ambient;
 
@@ -51,10 +48,9 @@ public class Phong implements Shader {
 		Iterator<LightSource> iterator = Lichter.iterator();
 
 		for (int i = 0; i < LichterAnzahl; i++) {
-			LichterArray[i] = iterator.next(); // stimmt so??
+			LichterArray[i] = iterator.next(); 
 		}
 
-		// Lichter.toArray(LichterArray);
 
 		Color help = inner.shade(hit, trace);
 
@@ -68,21 +64,12 @@ public class Phong implements Shader {
 			Color Idiffuse = Cl.mul(help).scale(
 					diffuse * Math.max(0, hit.getNormal().dot(v)));
 
-			if (!(trace.getScene().hit(new Ray(hit.getPoint(), v)).hits())) { // warum
-																				// funktioniert
-																				// ????
-																				// Warum
-																				// zählt
-																				// Lichtquelle
-																				// nicht
-																				// als
-																				// Treffer?
+			if (!(trace.getScene().hit(new Ray(hit.getPoint(), v)).hits())) { 
 				result = result.add(Idiffuse);
 			}
 
 		}
 
-		// neg invertiert vektor!
 
 		Vec3 r = trace.getRay().dir().neg().reflect(trace.getHit().getNormal())
 				.normalized();
@@ -97,10 +84,7 @@ public class Phong implements Shader {
 			Color Ispecular = Cl.scale(specular
 					* (float) Math.pow(Math.max(0, r.angle(v)), shininess));
 
-			if (!(trace.getScene().hit(new Ray(hit.getPoint(), v.neg())).hits())) { // warum
-																					// funktioniert
-																					// ????
-																					// s.o
+			if (!(trace.getScene().hit(new Ray(hit.getPoint(), v.neg())).hits())) { 
 				result = result.add(Ispecular);
 			}
 		}
